13e6b0d19b995137bae58688d16b56f075d60690
Alexandru <assatin112@gmail.com>
2023-09-29 13:39:32 +0300

    29.09.2023

--- second/personal_portfolio/help.py
+++ second/personal_portfolio/help.py
@@ -6,3 +6,14 @@
 #     description = models.CharField(max_length=250)
 #     image = models.ImageField(upload_to='skills/images/')
 #     url = models.URLField(blank=True)
+
+
+# blogs = Blog.objects.order_by('-date')
+#     -date - обратное по дате(сверху новое,внизу старое)
+#     order_by - сортирует по конкретному параметру
+
+##########################   ФИЛЬТРЫ DJANGO   ####################
+# |truncatechars: фильтр для ограничение символов на вывод странички
+# |striptags - убирает тэги html на страничках
+# |date:'d.m.Y' - настройка даты, в 'пишется свой формат'
+# |safe - обрабатывает все тэги, которые в тексте
